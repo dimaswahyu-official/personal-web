cc1b8e6dcee117d935a677ae710ea5bb748b05c3
Lenovo <dimas.u@inception.asia>
2021-03-01 15:20:35 +0700

    (ADD) Drawer Menu

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,34 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Anchor, Menu} from "antd";
 import image from '../../assets/images/logodw.png'
 
+import { Drawer, Button } from 'antd';
 const {Link} = Anchor;
 
-function AppHeader(){
-    return(
-        <div className="container-fluid">
-            <div className="header">
-                <div className="logo">
-                    <a href="/">
-                        <img alt="logo" src={image} height="60" width="65"/>
-                    </a>
-                </div>
-                {/*<Menu  theme="dark" mode="horizontal" defaultSelectedKeys={['Home']}>*/}
-                {/*    <Menu.Item key="Home">Home</Menu.Item>*/}
-                {/*    <Menu.Item key="About">About</Menu.Item>*/}
-                {/*    /!*<Menu.Item key="Achievement">Achievement</Menu.Item>*!/*/}
-                {/*    <Menu.Item key="Contact">Contact Me</Menu.Item>*/}
-                {/*</Menu>*/}
-                <Anchor targetOffset="65">
-                    <Link href="#home" title="Home"></Link>
-                    <Link href="#about" title="About"></Link>
-                    <Link href="#features" title="Features"></Link>
-                    <Link href="#works" title="Works"></Link>
+    function AppHeader() {
+        const [visible, setVisible] = useState(false);
 
-                </Anchor>
+        const showDrawer = () => {
+            setVisible(true);
+        };
+
+        const onClose = () => {
+            setVisible(false);
+        };
+        return (
+            <div className="container-fluid">
+                <div className="header">
+                    <div className="logo">
+                        <a href="/">
+                            <img alt="logo" src={image} height="60" width="65"/>
+                        </a>
+                    </div>
+                    <div className="mobileHidden">
+                        <Anchor targetOffset="65">
+                            <Link href="#home" title="Home"></Link>
+                            <Link href="#about" title="About"></Link>
+                            <Link href="#features" title="Features"></Link>
+                            <Link href="#works" title="Works"></Link>
+                        </Anchor>
+                    </div>
+                    <div className="mobileVisible">
+                        <Button type="primary" onClick={showDrawer}>
+                            <i class="fas fa-bars"></i>
+                        </Button>
+                        <Drawer
+                            title="Menu"
+                            placement="right"
+                            closable={false}
+                            onClose={onClose}
+                            visible={visible}
+                        >
+                            <Anchor targetOffset="65">
+                                <Link href="#home" title="Home"></Link>
+                                <Link href="#about" title="About"></Link>
+                                <Link href="#features" title="Features"></Link>
+                                <Link href="#works" title="Works"></Link>
+                            </Anchor>
+                        </Drawer>
+                    </div>
+                </div>
+
             </div>
-        </div>
         );
-}
+    }
 
 export default AppHeader;
